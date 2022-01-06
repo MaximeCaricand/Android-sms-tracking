7d791c96478e496f85be833612b9d9183eb7b4e0
hf171907 <florian.henry@etu.univ-lehavre.fr>
2022-01-06 18:18:51 +0100

    Création des premieres activitys et envoies de sms fonctionnel

--- app/src/main/java/com/example/mobiletracking/MainActivity.java
+++ app/src/main/java/com/example/mobiletracking/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.mobiletracking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    DBHelper dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.dbHandler = new DBHelper(getApplicationContext());
+    }
+
+    public void openPedometer(View view) {
+        Intent settingsIntent = new Intent(this, PedometerActivity.class);
+        startActivity(settingsIntent);
+    }
+
+    public void openTracker(View view) {
+        Intent settingsIntent = new Intent(this, TrackerActivity.class);
+        startActivity(settingsIntent);
+    }
+
+    public void clearDB(View view) {
+        this.dbHandler.clearTable();
     }
 }
---
+++ app/src/main/java/com/example/mobiletracking/PedometerActivity.java
@@ -0,0 +1,19 @@
+package com.example.mobiletracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class PedometerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedometer);
+
+
+    }
+
+}
