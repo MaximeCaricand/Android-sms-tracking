7cbf3543a9c01553f01d4603c47b2d04f3142116
maxime caricand <maxime.caricand@co-assist.fr>
2022-02-02 00:57:03 +0100

    update receiver and permissions

--- app/src/main/java/com/example/mobiletracking/MainActivity.java
+++ app/src/main/java/com/example/mobiletracking/MainActivity.java
@@ -4,26 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     DBHelper dbHandler;
-    private static final int MY_PERMISSION_REQUEST_CODE_SMS = 1;
-    private static final String SMS_PERMISSION = Manifest.permission_group.SMS;
+    private static final int PERMISSION_REQUEST_CODE = 2;
+    private static final String[] PERMISSION_LIST = {
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.RECEIVE_SMS,
+            Manifest.permission.READ_SMS
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         this.dbHandler = new DBHelper(getApplicationContext());
-        this.askPermissionAndSendSMS();
+        this.HandlePermissions();
     }
 
     public void openPedometer(View view) {
@@ -40,11 +45,16 @@ public class MainActivity extends AppCompatActivity {
         this.dbHandler.clearTable();
     }
 
-    private void askPermissionAndSendSMS() {
+    @SuppressLint("NewApi")
+    private void HandlePermissions() {
         if (android.os.Build.VERSION.SDK_INT >=  android.os.Build.VERSION_CODES.M) {
-            if (ActivityCompat.checkSelfPermission(this, SMS_PERMISSION) != PackageManager.PERMISSION_GRANTED) {
-                this.requestPermissions(new String[]{SMS_PERMISSION}, MY_PERMISSION_REQUEST_CODE_SMS);
-                return;
+
+            boolean needPermission = Arrays.stream(PERMISSION_LIST)
+                    .map((el) -> ActivityCompat.checkSelfPermission(this, el) != PackageManager.PERMISSION_GRANTED)
+                    .reduce(false, (cur, acc) -> acc || cur);
+
+            if (needPermission) {
+                this.requestPermissions(PERMISSION_LIST, PERMISSION_REQUEST_CODE);
             }
         }
     }
@@ -53,13 +63,12 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
-            case MY_PERMISSION_REQUEST_CODE_SMS: {
-                String context = "";
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                    context = "granted !";
-                else
-                    context = "denied !";
-                Toast.makeText(this, "Permission " + context, Toast.LENGTH_LONG).show();
+            case PERMISSION_REQUEST_CODE: {
+                Toast.makeText(
+                        this,
+                        "Permission " + ((grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) ? "granted" :"denied" + " !"),
+                        Toast.LENGTH_LONG
+                ).show();
                 break;
             }
         }
--- app/src/main/java/com/example/mobiletracking/TrackerActivity.java
+++ app/src/main/java/com/example/mobiletracking/TrackerActivity.java
@@ -1,14 +1,17 @@
 package com.example.mobiletracking;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsManager;
-import android.util.Log;
+import android.telephony.SmsMessage;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,27 +19,111 @@ import android.widget.Toast;
 
 public class TrackerActivity extends AppCompatActivity {
 
-    private final String message = "Acceptez-vous de vous faire suivre ? \n Envoyez 'Oui' pour lancer le suivi.";
-    private EditText editTextPhoneNumber;
+    // move this to android values
+    private final String startTrackingMsg = "start_tracking";
+    private final String stopTrackingMsg = "stop_tracking";
+
+    private EditText etPhoneNumber;
     private Button buttonSend;
+    private String trackedPhoneNumber;
+
+    private BroadcastReceiver smsReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker);
-        this.editTextPhoneNumber = this.findViewById(R.id.editText_phoneNumber);
+        this.etPhoneNumber = this.findViewById(R.id.editText_phoneNumber);
         this.buttonSend = this.findViewById(R.id.button_send);
-        this.buttonSend.setOnClickListener((View v) -> this.sendSMS_by_smsManager());
+        this.buttonSend.setOnClickListener((View v) -> this.updateTrackingState());
+        this.updateButtonText();
+
+        smsReceiver = new BroadcastReceiver() {
+            @Override
+            @SuppressLint("NewApi")
+            public void onReceive(Context context, Intent intent) {
+                Bundle bundle = intent.getExtras();
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                if (pdus != null) {
+                    for (Object pdu : pdus) {
+                        SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdu, bundle.getString("format"));
+                        Toast.makeText(context, smsMessage.getOriginatingAddress(), Toast.LENGTH_LONG).show();
+                        /*if (smsMessage.getOriginatingAddress().matches(trackedPhoneNumber)) {
+                            Toast.makeText(context, smsMessage.getMessageBody(), Toast.LENGTH_LONG).show();
+                        }*/
+                    }
+                }
+            }
+        };
+        registerReceiver(smsReceiver, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
     }
 
-    private void sendSMS_by_smsManager() {
-        String phoneNumber = this.editTextPhoneNumber.getText().toString();
+    @Override
+    protected void onPause() {
+        unregisterReceiver(smsReceiver);
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(this.trackingON()) {
+            this.askStopTracking();
+        }
+    }
+
+    protected void openHistory(View v) {
+
+    }
+
+    public void onSaveInstanceState(Bundle bundle) {
+        super.onSaveInstanceState(bundle);
+        bundle.putString("trackedPhoneNumber", this.trackedPhoneNumber);
+    }
+
+    public void onRestoreInstanceState(Bundle bundle) {
+        this.trackedPhoneNumber = bundle.getString("trackedPhoneNumber");
+        this.etPhoneNumber.setText(this.trackedPhoneNumber);
+        this.updateButtonText();
+    }
+
+    private boolean trackingON() {
+        System.out.println(this.trackedPhoneNumber);
+        return this.trackedPhoneNumber != null;
+    }
+
+    private void updateTrackingState() {
+        if (this.trackingON()) this.askStopTracking();
+        else this.askStartTracking();
+    }
+
+    private void askStartTracking() {
+        String phoneNumber = this.etPhoneNumber.getText().toString();
+        if (!phoneNumber.equals("")) {
+            this.trackedPhoneNumber = phoneNumber;
+            this.sendSMS(phoneNumber, this.startTrackingMsg);
+        }
+    }
+
+    private void askStopTracking() {
+        String lastPhoneNumber = this.trackedPhoneNumber;
+        this.trackedPhoneNumber = null;
+        this.sendSMS(lastPhoneNumber, this.stopTrackingMsg);
+    }
+
+    private void updateButtonText() {
+        if (this.trackingON()) this.buttonSend.setText("Arrêter le suivi");
+        else this.buttonSend.setText("Demander le suivi");
+    }
+
+    private void sendSMS(String phoneNumber, String message) {
         try {
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(phoneNumber, null, message, null, null);
-            Toast.makeText(getApplicationContext(), "Your sms has successfully sent!", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "SMS envoyé", Toast.LENGTH_LONG).show();
+            this.updateButtonText();
         } catch (Exception ex) {
-            Toast.makeText(getApplicationContext(), "Your sms has failed... " + ex.getMessage(), Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Erreur d'envoi de SMS" + ex.getMessage(), Toast.LENGTH_LONG).show();
             ex.printStackTrace();
         }
     }
