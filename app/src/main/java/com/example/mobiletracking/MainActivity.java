c03c965fc5f8c511a366bdd138e60ddca57e3f0d
maxime caricand <maxime.caricand@co-assist.fr>
2022-02-01 17:02:53 +0100

    update permissions

--- app/src/main/java/com/example/mobiletracking/MainActivity.java
+++ app/src/main/java/com/example/mobiletracking/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.mobiletracking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DBHelper dbHandler;
+    private static final int MY_PERMISSION_REQUEST_CODE_SMS = 1;
+    private static final String SMS_PERMISSION = Manifest.permission_group.SMS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.dbHandler = new DBHelper(getApplicationContext());
+        this.askPermissionAndSendSMS();
     }
 
     public void openPedometer(View view) {
@@ -30,5 +38,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void clearDB(View view) {
         this.dbHandler.clearTable();
+    }
+
+    private void askPermissionAndSendSMS() {
+        if (android.os.Build.VERSION.SDK_INT >=  android.os.Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, SMS_PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+                this.requestPermissions(new String[]{SMS_PERMISSION}, MY_PERMISSION_REQUEST_CODE_SMS);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_CODE_SMS: {
+                String context = "";
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    context = "granted !";
+                else
+                    context = "denied !";
+                Toast.makeText(this, "Permission " + context, Toast.LENGTH_LONG).show();
+                break;
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mobiletracking/SMSReceiver.java
+++ app/src/main/java/com/example/mobiletracking/SMSReceiver.java
@@ -17,38 +17,23 @@ public class SMSReceiver extends BroadcastReceiver {
     @TargetApi(Build.VERSION_CODES.M)
     @Override
     public void onReceive(Context context, Intent intent) {
-        // Get the SMS message.
         Bundle bundle = intent.getExtras();
         SmsMessage[] msgs;
         String strMessage = "";
-        String format = bundle.getString("format");
-
-        // Retrieve the SMS message received.
         Object[] pdus = (Object[]) bundle.get(pdu_type);
-
         if (pdus != null) {
-            // Check the Android version.
-            boolean isVersionM = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M);
-
             // Fill the msgs array.
             msgs = new SmsMessage[pdus.length];
             for (int i = 0; i < msgs.length; i++) {
                 // Check Android version and use appropriate createFromPdu.
-                if (isVersionM) {
-                    // If Android version M or newer:
-                    msgs[i] =
-                            SmsMessage.createFromPdu((byte[]) pdus[i], format);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i], bundle.getString("format"));
                 } else {
-                    // If Android version L or older:
                     msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
                 }
-
-                // Build the message to show.
                 strMessage += "SMS from " + msgs[i].getOriginatingAddress();
                 strMessage += " :" + msgs[i].getMessageBody() + "\n";
-
-                // Log and display the SMS message.
-                Log.d(TAG, "onReceive: " + strMessage);
+                System.out.println("onReceive: " + strMessage);
                 Toast.makeText(context, strMessage, Toast.LENGTH_LONG).show();
             }
         }
